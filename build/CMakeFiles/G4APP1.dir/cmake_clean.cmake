3c51e334d56a6a1d9f52dfc65a2b12856e5bb771
buddhasystem <potekhin@bnl.gov>
2023-06-01 22:09:16 -0400

    First commit

---
+++ build/CMakeFiles/G4APP1.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/G4APP1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/G4APP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
